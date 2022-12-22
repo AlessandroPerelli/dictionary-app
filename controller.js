84cc3f2e51cb56492f897bb68cf01b3ebc3b5288
Alessandro-Perelli-University <aperelli1@sheffield.ac.uk>
2022-12-22 21:57:44 +0000

    finished fetching api data from react app

--- controller.js
+++
@@ -1,6 +0,0 @@
-const request = require('request')
-
-request("https://api.dictionaryapi.dev/api/v2/entries/en/hello", {json : true}, (err, res, body) => {
-    if (err) {return console.log(err)}
-    console.log(body)
-})
--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = (await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/beach")).json()
+      setData(result)
+      console.log(result[2])
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
