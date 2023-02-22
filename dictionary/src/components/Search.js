8d3bc630a8103186ebb03db4cd1513ccd2a39d22
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-02-22 17:08:35 +0000

    progress: added routing functionality to search button

--- dictionary/src/components/Search.js
+++ dictionary/src/components/Search.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { css, Text, Input } from '@nextui-org/react';
+import { Link } from "react-router-dom";
+import { css, Text, Input, Button } from '@nextui-org/react';
 import './search.css'
 
 export default function Search() {
@@ -8,6 +9,7 @@ export default function Search() {
         <Text>Search for a word</Text>
         <Text>Enter the word you would like to discover the defnition of</Text>
         <Input placeholder='Find interesting words...' clearable='true' fullWidth='true' />
+        <Link to="/result"><Button>Search</Button></Link>
     </div>
   )
 }
